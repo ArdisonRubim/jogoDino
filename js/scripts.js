493e24c2746a0cbba4a1a26e66682589b056b387
ArdisonRubim <ardison.rubim@gmail.com>
2021-10-03 23:59:00 -0300

    imagens, estrutura estao sendo definidas

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+const dino = document.querySelector('.dino')
+
+document.addEventListener('keyup', (event) => {
+    if(event.DOM_KEY_LOCATION_NUMPAD == 32) {
+        console.log("certo")
+    }
+})
